6dbebe6705ec27bc01f5ab00a436bcc06277dc28
NaokiWindows <gtvofromb@gmail.com>
2024-03-14 16:39:06 +0900

    main作成

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String str;
+		System.out.println("Hello");
+		InputCommand inp = new InputCommand();
+		str = inp.getInput();
+		System.out.println(str);
+	}
+}
