0ae423ec0bc0f2c8abc779d460d66a21585cd92a
NaokiWindows <gtvofromb@gmail.com>
2024-03-14 17:14:07 +0900

    だんだんわかってきたね

---
+++ src/InputHandler.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//標準入力を扱うクラス
+public class InputHandler {
+	
+	public Scanner scanner;
+	
+	public InputHandler() {
+		this.scanner = new Scanner(System.in);
+	}
+	
+	public String readLine() {
+		return scanner.nextLine();
+	}
+	
+	public void close() {
+		scanner.close();
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		String str;
 		System.out.println("Hello");
-		InputCommand inp = new InputCommand();
-		str = inp.getInput();
-		System.out.println(str);
+		InputHandler inp = new InputHandler();
+		System.out.println(inp.readLine());
 	}
 }
